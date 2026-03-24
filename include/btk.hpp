4f508e65c5eb2a8a475fb4e59d6e08d407a675db
Piotr Dabrowski <piotrdabrowski@nudne.studio>
2026-03-24 17:52:35 +0100

    [Code] btk command "add"

--- include/btk.hpp
+++ include/btk.hpp
@@ -1,20 +1,37 @@
 #pragma once
 
+#include <unordered_map>
+#include <functional>
+
 #include <nudny/util/cli_parser.hpp>
 #include <collection.hpp>
 
 namespace btk {
 	class Biblioteka {
-		protected:
-			nudny::util::CliParser parser;
-			std::vector<std::shared_ptr<Collection>> collection;
-
-			void handleEvent();
 		public:
 			Biblioteka() = default;
 			Biblioteka(const Biblioteka&) = delete;
 			Biblioteka(int t_argc, char* t_argv[]);
 			~Biblioteka() = default;
+		protected:
+
+			std::string defaultCollctionDir{"~/.btk/collections/"};
+			std::shared_ptr<nudny::util::CliParser> parser;
+			nudny::util::Commander cmd;
+			std::vector<std::shared_ptr<Collection>> collection;
+			
+			
+			void handleEvent();
+			void loadDir(const std::string& t_dirPath);
+
+			void add();
+			void findBook();
+			
+		private:
+			std::unordered_map<std::string, std::function<void()>> actions{
+				{"add", [this](){ add();} },
+			};
+
 	};
 
 	int btk(int t_argc, char* t_argv[]);
--- src/btk.cpp
+++ src/btk.cpp
@@ -1,18 +1,49 @@
+#include <iostream>
+#include <filesystem>
+
 #include <btk.hpp>
 
 namespace btk {
-	Biblioteka::Biblioteka(int t_argc, char* t_argv[])
-		: parser{t_argc, t_argv} {
+	Biblioteka::Biblioteka(int t_argc, char* t_argv[]) 
+		: parser{std::make_shared<nudny::util::CliParser>(t_argc, t_argv)}	{
+			cmd.setCli(parser);
 			handleEvent();
 		}
 
 	void Biblioteka::handleEvent() {
-		if(parser.exist("l")) {
-			std::cout << "Loading collection " << parser.get("l") << "\n";
-			//collection.push_back(std::make_shared<Collection>(parser.get("load")));
-		}
+		if(auto it = actions.find(cmd.cli(0)); it != actions.end()) {
+				it->second();
+		} else {
+			std::cout << "Unknown command\n";
+		}	
 	}	
+
+	void Biblioteka::loadDir(const std::string& t_dirPath) {
+		/*
+		 * if(dir not empty) {
+		 * 	for(each file in dir) {
+		 * 		if(file is collection) {
+		 * 			shared<Col> c = make_shar;
+		 * 			if(c->load(file)) {
+		 * 				collections.push_back(c)
+		 * 				cout "collection <name> loaded"
+		 * 				return 0;
+		 * 			} else {
+		 * 				cout "could not load <name> collection;
+		 * 				return 2;
+		 * 			}
+		 * 		}
+		 * 	}
+		 * }
+		 * cout "dir empty"
+		 * return 1;
+		 */
+	}
 	
+	void Biblioteka::add() {
+		std::cout << "Add book\n";
+	}
+
 	int btk(int t_argc, char* t_argv[]) {
 		Biblioteka b(t_argc, t_argv);
 		return 0;
